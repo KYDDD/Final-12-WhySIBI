3ea9c7f0e8a665e7943c3509b293b768b88da954
KYDDD <riderkim88@gmail.com>
2025-07-14 01:21:28 +0000

    🔧 set: components폴더 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import ButtonBasic from '@/components/Button_basic';
+
 export default function Home() {
-  return <h1>final-project-sample v02안녕안녕 메롱</h1>;
+  return (
+    <>
+      <h1 className="bg-red-500">final-project-sample v02안녕안녕 메롱</h1>
+      <ButtonBasic></ButtonBasic>
+    </>
+  );
 }
---
+++ src/components/Button_basic.tsx
@@ -0,0 +1,9 @@
+function ButtonBasic() {
+  return (
+    <>
+      <button className="bg-flame-300">장바구니</button>
+    </>
+  );
+}
+
+export default ButtonBasic;
